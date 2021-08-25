59b825044b0cc8a37db2ac67145bd490d720fa3e
Thomas Sankara <thomassankara74@gmail.com>
2021-08-25 15:34:11 +0300

    Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import Button from "./Button"
+import hero from "../hero.svg"
+
+export default function Hero() {
+  return (
+    <>
+      <section className="hero">
+        <div className="overlay">
+          <article>
+            <h1>Solve trigonometry problems on the fly</h1>
+            <Button />
+          </article>
+
+          <article>
+            <img src={hero} alt="" />
+          </article>
+        </div>
+      </section>
+    </>
+  )
+}
