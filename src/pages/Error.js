10376ba69c941029e2e506c4ffa68532503b9872
Thomas Sankara <thomassankara74@gmail.com>
2021-09-01 08:02:09 +0300

    error page

---
+++ src/pages/Error.js
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom"
+
+export default function Error() {
+  return (
+    <div>
+      <h1>Error page</h1>
+      <NavLink to="/">Back to homepage</NavLink>
+    </div>
+  )
+}
