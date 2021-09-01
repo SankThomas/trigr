af74600a85c916c694813fd5a7edab8777c3c7dc
Thomas Sankara <thomassankara74@gmail.com>
2021-09-01 08:02:39 +0300

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import GetStarted from "./pages/GetStarted"
+import ContactUs from "./pages/ContactUs"
+import AboutUs from "./pages/AboutUs"
+import Error from "./pages/Error"
 
 function App() {
   return (
-    <main className="wrapper">
+    <BrowserRouter>
+      {/* <main className="wrapper">
       <Header />
       <Hero />
       <GetStarted />
-    </main>
+    </main> */}
+      <main className="wrapper">
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Hero />
+          </Route>
+          <Route path="/contact-us">
+            <ContactUs />
+          </Route>
+          <Route path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route path="/get-started">
+            <GetStarted />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   )
 }
 
