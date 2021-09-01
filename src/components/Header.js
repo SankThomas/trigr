a38d143d667b86f5d9cc98ae74b7048e1fe695fa
Thomas Sankara <thomassankara74@gmail.com>
2021-09-01 08:01:24 +0300

    style logo + add NavLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import Button from "./Button"
 
 export default function Header() {
@@ -5,13 +6,19 @@ export default function Header() {
     <>
       <header className="header">
         <div className="logo">
-          <h2>Trigr</h2>
+          <h2 className="font-bold text-2xl lg:text-4xl">
+            <NavLink to="/">Trigr</NavLink>
+          </h2>
         </div>
 
         <nav>
           <ul>
-            <li>About Us</li>
-            <li>Let's talk</li>
+            <li>
+              <NavLink to="/about-us">About Us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact-us">Let's talk</NavLink>
+            </li>
           </ul>
         </nav>
 
