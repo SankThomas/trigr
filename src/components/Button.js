f64a8ab68b3dddf5bf58396e698185ba3b6af6d6
Thomas Sankara <thomassankara74@gmail.com>
2021-09-04 08:54:02 +0300

    add activeClassName

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,8 +4,12 @@ export default function Button() {
   return (
     <>
       <div className="cta">
-        <NavLink to="/get-started">
-          <button className="btn">Get Started</button>
+        <NavLink
+          to="/get-started"
+          activeClassName="activeBtn"
+          className="bg-green-500 py-2 px-6 rounded shadow transition-all duration-300 hover:bg-transparent hover:text-green-500 border border-green-500"
+        >
+          <button>Get Started</button>
         </NavLink>
       </div>
     </>
