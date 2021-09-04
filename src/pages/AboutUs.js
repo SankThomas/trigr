3d3a8bded1df9857d4ea553541b9de69ac03e48f
Thomas Sankara <thomassankara74@gmail.com>
2021-09-04 08:54:17 +0300

    About us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,28 @@
+import about from "../about.svg"
+
 export default function AboutUs() {
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+    <>
+      <section className="py-10 px-5 md:mt-20">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:items-center lg:gap-10">
+          <article>
+            <img src={about} alt="" />
+          </article>
+
+          <article>
+            <h1 className="font-bold text-4xl mb-5 mt-10 md:mt-0">
+              Why does Trigr exist?
+            </h1>
+            <p>
+              Now led tedious shy lasting females off. Dashwood marianne in of
+              entrance be on wondered possible building. Wondered sociable he
+              carriage in speedily margaret. Up devonshire of he thoroughly
+              insensible alteration. An mr settling occasion insisted distance
+              ladyship so.
+            </p>
+          </article>
+        </div>
+      </section>
+    </>
   )
 }
