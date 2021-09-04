07cb5cfc308b40c16d7b5e44e17e622d4c50c56f
Thomas Sankara <thomassankara74@gmail.com>
2021-09-04 08:54:06 +0300

    add activeClassName

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,23 @@ export default function Header() {
       <header className="header">
         <div className="logo">
           <h2 className="font-bold text-2xl lg:text-4xl">
-            <NavLink to="/">Trigr</NavLink>
+            <NavLink to="/" exact activeClassName="text-green-500">
+              Trigr
+            </NavLink>
           </h2>
         </div>
 
         <nav>
           <ul>
             <li>
-              <NavLink to="/about-us">About Us</NavLink>
+              <NavLink to="/about-us" activeClassName="text-green-500">
+                About Us
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/contact-us">Let's talk</NavLink>
+              <NavLink to="/contact-us" activeClassName="text-green-500">
+                Let's talk
+              </NavLink>
             </li>
           </ul>
         </nav>
