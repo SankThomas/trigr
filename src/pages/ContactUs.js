bb5fd9e68cb7aa572e65f02d0d5f24be6841f09e
Thomas Sankara <thomassankara74@gmail.com>
2021-09-04 09:18:18 +0300

    Contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,7 +1,52 @@
+import contact from "../contact.svg"
+
 export default function ContactUs() {
   return (
-    <div>
-      <h1>Contact Us</h1>
-    </div>
+    <>
+      <section className="py-10 px-5">
+        <div className="flex flex-col-reverse md:grid grid-cols-2 gap-10">
+          <article>
+            <h1 className="text-4xl mb-5 mt-10 font-bold">Let's talk some</h1>
+            <form className="flex flex-col">
+              <label htmlFor="name">What is your name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Your name"
+                required
+              />
+              <label htmlFor="email">Enter your email address</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                required
+              />
+              <label htmlFor="textarea">I want us to talk about...</label>
+              <textarea
+                name="message"
+                id="textarea"
+                cols="30"
+                rows="10"
+                placeholder="I want us to talk about..."
+                required
+              ></textarea>
+              <button
+                type="submit"
+                className="block text-left mt-5 py-2 px-6 rounded-lg shadow text-white"
+              >
+                Send
+              </button>
+            </form>
+          </article>
+
+          <article>
+            <img src={contact} alt="" />
+          </article>
+        </div>
+      </section>
+    </>
   )
 }
