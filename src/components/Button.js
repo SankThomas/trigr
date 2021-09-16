7532628e4d004e0a6bbea3a8aecd512d5b601fea
Thomas Sankara <thomassankara74@gmail.com>
2021-09-16 14:42:28 +0300

    login function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,19 @@
+import { useContext } from "react"
 import { NavLink } from "react-router-dom"
+import AuthContext from "../context/auth"
 
 export default function Button() {
+  const { login, logout } = useContext(AuthContext)
+
   return (
     <>
       <div className="cta">
-        <NavLink
-          to="/get-started"
-          activeClassName="activeBtn"
+        <button
           className="bg-green-500 py-2 px-6 rounded shadow transition-all duration-300 hover:bg-transparent hover:text-green-500 border border-green-500"
+          onClick={login}
         >
-          <button>Get Started</button>
-        </NavLink>
+          Log In
+        </button>
       </div>
     </>
   )
