d75a1ca801f619b7d4fd3ee46a39844604ea046a
Thomas Sankara <thomassankara74@gmail.com>
2021-09-16 15:31:50 +0300

    login function for home component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,10 @@
-import Button from "./Button"
+import { useContext } from "react"
+import AuthContext from "../context/auth"
 import hero from "../hero.svg"
 
 export default function Hero() {
+  const { login } = useContext(AuthContext)
+
   return (
     <>
       <section className="hero">
@@ -10,7 +13,12 @@ export default function Hero() {
             <h1 className="font-bold">
               Solve trigonometry problems on the fly
             </h1>
-            <Button />
+            <button
+              className="bg-green-500 py-2 px-6 rounded shadow transition-all duration-300 hover:bg-transparent hover:text-green-500 border border-green-500"
+              onClick={login}
+            >
+              Get Started
+            </button>
           </article>
 
           <article>
